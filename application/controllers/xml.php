5c83328c81d4f72f53a4b1bc1d1ea752bd841e37
Sara <saraalamillo93@gmail.com>
2015-03-05 15:25:42 +0100

    Finalizada la exportación en XML.

--- application/controllers/xml.php
+++ application/controllers/xml.php
@@ -12,49 +12,45 @@ class Xml extends Sara {
     }
 
     public function exportar() {
-        if ($this->input->post()) {
-            $this->form_validation->set_rules('ruta', 'usuario', 'callback_ruta_check');
 
-            if ($this->form_validation->run()) {
-                $categorias = $this->productos_model->listar_categorias();
 
-                $xml = "<?xml version=\"1.0\" ?>\n"
-                        . "<categorias>\n";
-                foreach ($categorias as $c) {
-                    $xml .= "<categoria>\n"
-                            . "<id>{$c->id}</id>\n"
-                            . "<nombre>{$c->nombre}</nombre>\n";
-                    $productos = $this->productos_model->listar_productos($c->id);
-                    if (count($productos) != 0) {
-                        $xml .= "<productos>\n";
-                        foreach ($productos as $p) {
-                            $xml .= "<producto>\n";
-                            foreach ($p as $key => $value) {
-                                $xml .= "<$key>$value</$key>\n";
-                            }
-                            $xml .= "</producto>\n";
-                        }
-                        $xml .= "</productos>\n";
-                    } else {
-                        $xml .= "<productos />\n";
+        $xml = "<?xml version=\"1.0\" ?>\n"
+                . "<categorias>\n";
+        foreach ($categorias as $c) {
+            $xml .= "<categoria>\n"
+                    . "<id>{$c->id}</id>\n"
+                    . "<nombre>{$c->nombre}</nombre>\n";
+            $productos = $this->productos_model->listar_productos($c->id);
+            if (count($productos) != 0) {
+                $xml .= "<productos>\n";
+                foreach ($productos as $p) {
+                    $xml .= "<producto>\n";
+                    foreach ($p as $key => $value) {
+                        $xml .= "<$key>$value</$key>\n";
                     }
-                    $xml .= "</categoria>\n";
+                    $xml .= "</producto>\n";
                 }
-                $xml .= "</categorias>";
-
-                $nombre_fichero = "datos_tienda_" . date("dmY") . ".xml";
-
-                file_put_contents($this->input->post('ruta') . $nombre_fichero, $xml);
-                
-                $mensaje = "Se ha exportado correctamente los productos de la tienda";
+                $xml .= "</productos>\n";
+            } else {
+                $xml .= "<productos />\n";
             }
+            $xml .= "</categoria>\n";
         }
+        $xml .= "</categorias>";
+
+        $nombre_fichero = "datos_tienda_" . date("dmY") . ".xml";
+        $ruta = APPPATH . "/tmp/";
+
+        file_put_contents( $ruta . $nombre_fichero, $xml);
+
+        header('Content-Type: application/x-download');
+        header('Content-Disposition: attachment; filename="' . $nombre_fichero . '"');
+        header('Cache-Control: private, max-age=0, must-revalidate');
+        header('Pragma: public');
         
-        $contenido = $this->load->view("exportar_xml", "", TRUE);
         
-        
-        $this->vista($contenido);
-        
+        unlink($ruta . $nombre_fichero);
+
     }
 
     public function importar() {
